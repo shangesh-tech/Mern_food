c745d8918d4f04ee8b1ba1e0b47938cb81137235
Shangesh S <shangesh2006@gmail.com>
2024-09-18 20:21:49 +0530

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -27,14 +27,15 @@ const Header = () => {
   };
 
   const user = async () => {
-    try {
-      const response = await axios.get("/api/v1/me");
-      console.log("user32=>",response.data)
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const response = await axios.get("/api/v1/me", { withCredentials: true });
+    console.log("user32=>", response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
   const isAuth_ = async () => {
     const isAuthenticated = await checkAuth();
